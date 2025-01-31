54ac16fa742570d258f0730fb15529080d6a1ed6
Liminghing <im349412@gmail.com>
2025-01-31 14:29:28 +0900

    ISSUE-8[feat] QR 스캐너 세팅

--- app/src/main/java/com/jkweyu/quickqr/view/home/HomeFragment.kt
+++ app/src/main/java/com/jkweyu/quickqr/view/home/HomeFragment.kt
@@ -1,8 +1,12 @@
 package com.jkweyu.quickqr.view.home
 
+import android.util.Log
 import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.lifecycle.Observer
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions
+import com.google.mlkit.vision.codescanner.GmsBarcodeScanning
 import com.jkweyu.quickqr.R
 import com.jkweyu.quickqr.base.BaseFragment
 import com.jkweyu.quickqr.databinding.FragmentHomeBinding
@@ -22,8 +26,15 @@ class HomeFragment(private val mainViewModel: MainViewModel): BaseFragment<Fragm
         val VIEW_TYPE_EMPTY = 5
     }
     private lateinit var homeViewModel: HomeRVItemViewModel
+    val options = GmsBarcodeScannerOptions.Builder()
+        .setBarcodeFormats(
+            Barcode.FORMAT_QR_CODE,
+            Barcode.FORMAT_AZTEC)
+        .build()
 
     override fun initView() {
+        val scanner = GmsBarcodeScanning.getClient(requireContext())
+
         //뷰모델 생성
         homeViewModel = HomeRVItemViewModel(mainViewModel.homeRvItemList.value!!)
         binding.apply {
@@ -52,7 +63,20 @@ class HomeFragment(private val mainViewModel: MainViewModel): BaseFragment<Fragm
             homeViewModel.selectedItem.observe(this@HomeFragment, Observer { item ->
                 when(item?.itemType){
                     VIEW_TYPE_CREATE_QR -> {
-                        Toast.makeText(this@HomeFragment.context,"qr 스캔하기}",Toast.LENGTH_SHORT).show()
+                        scanner.startScan()
+                            .addOnSuccessListener { barcode ->
+                                // Task completed successfully
+                                val rawValue: String? = barcode.rawValue
+                                Log.d("qqqqqqq",rawValue.toString())
+                            }
+                            .addOnCanceledListener {
+                                // Task canceled
+                                Log.d("qqqqqqq","addOnCanceledListener")
+                            }
+                            .addOnFailureListener { e ->
+                                // Task failed with an exception
+                                Log.d("qqqqqqq","addOnFailureListener")
+                            }
                     }
                     VIEW_TYPE_SCAN_QR -> {
                         Toast.makeText(this@HomeFragment.context,"qr 생성하기}",Toast.LENGTH_SHORT).show()
@@ -82,4 +106,5 @@ class HomeFragment(private val mainViewModel: MainViewModel): BaseFragment<Fragm
             })
         }
     }
+
 }

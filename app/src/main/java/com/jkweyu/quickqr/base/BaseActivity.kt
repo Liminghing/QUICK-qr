2b9cc6c36b9165d28e62d453f8d0f958dd47edb1
Liminghing <im349412@gmail.com>
2025-03-10 12:50:02 +0900

    clean code

--- app/src/main/java/com/jkweyu/quickqr/base/BaseActivity.kt
+++ app/src/main/java/com/jkweyu/quickqr/base/BaseActivity.kt
@@ -26,10 +26,6 @@ abstract class BaseActivity<T: ViewDataBinding>(@LayoutRes val layoutRes: Int)
         binding.lifecycleOwner = this@BaseActivity
         ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, windowInsets ->
             val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
-            // Apply the insets as a margin to the view. This solution sets
-            // only the bottom, left, and right dimensions, but you can apply whichever
-            // insets are appropriate to your layout. You can also update the view padding
-            // if that's more appropriate.
             v.setPadding(0,insets.top,0,0)
 
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
